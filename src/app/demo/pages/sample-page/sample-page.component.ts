c245a0af04329b3f6668102a64d7d7b9ea98b969
Bansari Shah <106529462+Bansari258@users.noreply.github.com>
2023-03-21 07:30:04 +0530

    report

--- src/app/demo/pages/sample-page/sample-page.component.ts
+++ src/app/demo/pages/sample-page/sample-page.component.ts
@@ -1,13 +1,34 @@
 import { Component, OnInit } from '@angular/core';
+import {SeoAnalyticChart1} from './chart/seo-analytic-chart-1';
+import {SeoAnalyticChart2} from './chart/seo-analytic-chart-2';
+import {SeoAnalyticChart3} from './chart/seo-analytic-chart-3';
+import {SeoAnalyticChart4} from './chart/seo-analytic-chart-4';
 
 @Component({
+
   selector: 'app-sample-page',
   templateUrl: './sample-page.component.html',
   styleUrls: ['./sample-page.component.scss']
 })
 export class SamplePageComponent implements OnInit {
 
-  constructor() { }
+  public seoChartData1: any;
+  public seoChartData2: any;
+  public seoChartData3: any;
+  public seoChartData4: any;
+
+  public amountProcessedChartData: any;
+  public amountSpentChartData: any;
+  public profitProcessedChartData: any;
+
+  
+  constructor() {
+    this.seoChartData1 = SeoAnalyticChart1.seoChartData;
+    this.seoChartData2 = SeoAnalyticChart2.seoChartData;
+    this.seoChartData3 = SeoAnalyticChart3.seoChartData;
+    this.seoChartData4 = SeoAnalyticChart4.seoChartData;
+   
+  }
 
   ngOnInit() {
   }
